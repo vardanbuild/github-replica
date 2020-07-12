36db1e73f0e02d99f2fae5fe28b771465b8ee439
Vardan Bansal <vardan.bansal@harness.io>
2020-07-13 00:08:21 +0530

    reverted mocked api calls

--- src/services/ProfileService.js
+++ src/services/ProfileService.js
@@ -1,14 +1,14 @@
 import xhr from 'xhr-async';
 import mockRespose from './mocks/profile-service-mock.json';
 
-// export function getProfileInfo(){
-//     const url = 'https://api.github.com/users/supreetsingh247'
-//     return xhr.get(url)
-// }
-
 export function getProfileInfo() {
-  return {
-    response: mockRespose,
-    error: null,
-  };
+  const url = 'https://api.github.com/users/supreetsingh247';
+  return xhr.get(url);
 }
+
+// export function getProfileInfo() {
+//   return {
+//     response: mockRespose,
+//     error: null,
+//   };
+// }
--- src/services/RepositoryService.js
+++ src/services/RepositoryService.js
@@ -1,14 +1,14 @@
 import xhr from 'xhr-async';
 import mockRespose from './mocks/repo-service-mock.json';
 
-// export function getRepositories() {
-//   const url = 'https://api.github.com/users/supreetsingh247/repos';
-//   return xhr.get(url);
-// }
-
 export function getRepositories() {
-  return {
-    response: mockRespose,
-    error: null,
-  };
+  const url = 'https://api.github.com/users/supreetsingh247/repos';
+  return xhr.get(url);
 }
+
+// export function getRepositories() {
+//   return {
+//     response: mockRespose,
+//     error: null,
+//   };
+// }
