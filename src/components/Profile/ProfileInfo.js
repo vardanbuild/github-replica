7fe5f7ba95b627c02e9023619947aea2e3971f67
Vardan Bansal <vardan.bansal@harness.io>
2020-07-12 15:19:29 +0530

    Added profile section changes

--- src/components/Profile/ProfileInfo.js
+++ src/components/Profile/ProfileInfo.js
@@ -1,29 +1,48 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
 
 // import {ProfileService} from 'services'
-import * as ProfileService from '../../services/ProfileService'
+import * as ProfileService from '../../services/ProfileService';
 
 import './ProfileInfo.css';
 
 const ProfileInfo = () => {
+  const [profileInfo, setProfileInfo] = useState({});
 
-    const [profileInfo, setProfileInfo] = useState({})
+  const renderProfile = (profileInfo) => {
+    const { name, login, bio, company, location, avatar_url } = profileInfo;
+    return (
+      <div className="about">
+        <img src={avatar_url} alt="Avatar" className="avatar item" />
+        <span className="item">{name}</span>
+        <span className="item">{login}</span>
+        <span className="item">{bio}</span>
+        <button
+          type="button"
+          name=""
+          onClick={() => {}}
+          className="editProfileBtn item"
+        >
+          Edit Profile
+        </button>
+        <span className="item">{company}</span>
+        <span className="item">{location}</span>
+      </div>
+    );
+  };
 
-    useEffect(() => {
-        const getProfileInfo = async () => {
-            const {response, error} = await ProfileService.getProfileInfo()
-            if(!error){
-                setProfileInfo(response)
-            }else{
-                //show error
-            }
-        }
-        getProfileInfo()
-    }, [])
+  useEffect(() => {
+    const getProfileInfo = async () => {
+      const { response, error } = await ProfileService.getProfileInfo();
+      if (!error) {
+        setProfileInfo(response);
+      } else {
+        //show error
+      }
+    };
+    getProfileInfo();
+  }, []);
 
-    return(
-        <div className="main">Profile</div>
-    )
-}
+  return <div className="main">{renderProfile(profileInfo)}</div>;
+};
 
-export default ProfileInfo
+export default ProfileInfo;
